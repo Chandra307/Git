7374a4c5bbe95deb97f4431d7a80b11eca8ab437
Chandra307 <chandra19151425@gmail.com>
2023-11-17 22:01:19 +0530

    integrate signup backend

--- Task1/group-chat/back-end/app.js
+++ Task1/group-chat/back-end/app.js
@@ -7,12 +7,16 @@ const sequelize = require('./util/database');
 const userRoute = require('./routes/user');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    credentials: true,
+}));
 app.use(express.json());
 app.use(bodyParser.json({ extended: false}));
 app.use('/user', userRoute);
 
 sequelize.sync()
 // sequelize.sync({ force: true })
-.then(_ => app.listen(3000))
+.then(_ => app.listen(5000))
 .catch(err => console.log(err, 'in sync'));
--- Task1/group-chat/back-end/controllers/user.js
+++ Task1/group-chat/back-end/controllers/user.js
@@ -13,7 +13,7 @@ exports.postUser = async (req, res, next) => {
     try {
         let { name, email, phone, password } = req.body;
         if (isInputInvalid(name) || isInputInvalid(phone) || isInputInvalid(email) || isInputInvalid(password)) {
-            return res.status(400).json('Please fill all the input fields!');
+            return res.status(400).json({ message: 'Please fill all the input fields!' });
         }
         else {
             bcrypt.hash(password, 10, async (err, hash) => {
@@ -29,11 +29,11 @@ exports.postUser = async (req, res, next) => {
                         email,
                         password
                     });
-                    res.status(201).json({ message: 'User creation  successfull', user: newUser });
+                    res.status(201).json({ message: 'User creation successfull :)', user: newUser });
                 }
                 catch (err) {
                     // console.log(err, '500: server error');
-                    res.status(500).json({ message: 'A user with this e-mail already exists!', Error: err });
+                    res.status(500).json({ message: 'A user with this e-mail already exists.Please login!', Error: err });
                 }
             })
         }
