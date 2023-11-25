e100e141d525aa68740a1e844e5522ee79f73952
Chandra307 <chandra19151425@gmail.com>
2023-11-26 01:29:30 +0530

    admin superpowers

--- Task1/group-chat/back-end/routes/group.js
+++ Task1/group-chat/back-end/routes/group.js
@@ -6,5 +6,8 @@ const groupController = require('../controllers/group');
 router.post('/participants', authenticate, groupController.knowParticipants);
 router.get('/chats', authenticate, groupController.getChats);
 router.post('/newMsg', authenticate, groupController.sendMsg);
+router.post('/addParticipants', authenticate, groupController.addParticipants);
+router.put('/addAdmin/:id', authenticate, groupController.addAdmin);
+router.delete('/removeParticipant/:id', authenticate, groupController.removeParticipant);
 
 module.exports = router;
